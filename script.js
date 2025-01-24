bb8310980574e3ee179446be7ea71dd517b02a56
tottenilsson05 <abbedalle@gmail.com>
2025-01-24 18:59:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const brailleDigits = "⠀⢀⠠⢠⠐⢐⠰⢰⠈⢈⠨⢨⠘⢘⠸⢸⡀⣀⡠⣠⡐⣐⡰⣰⡈⣈⡨⣨⡘⣘⡸⣸⠄⢄⠤⢤⠔⢔⠴⢴⠌⢌⠬⢬⠜⢜⠼⢼⡄⣄⡤⣤⡔⣔⡴⣴⡌⣌⡬⣬⡜⣜⡼⣼⠂⢂⠢⢢⠒⢒⠲⢲⠊⢊⠪⢪⠚⢚⠺⢺⡂⣂⡢⣢⡒⣒⡲⣲⡊⣊⡪⣪⡚⣚⡺⣺⠆⢆⠦⢦⠖⢖⠶⢶⠎⢎⠮⢮⠞⢞⠾⢾⡆⣆⡦⣦⡖⣖⡶⣶⡎⣎⡮⣮⡞⣞⡾⣾⠁⢁⠡⢡⠑⢑⠱⢱⠉⢉⠩⢩⠙⢙⠹⢹⡁⣁⡡⣡⡑⣑⡱⣱⡉⣉⡩⣩⡙⣙⡹⣹⠅⢅⠥⢥⠕⢕⠵⢵⠍⢍⠭⢭⠝⢝⠽⢽⡅⣅⡥⣥⡕⣕⡵⣵⡍⣍⡭⣭⡝⣝⡽⣽⠃⢃⠣⢣⠓⢓⠳⢳⠋⢋⠫⢫⠛⢛⠻⢻⡃⣃⡣⣣⡓⣓⡳⣳⡋⣋⡫⣫⡛⣛⡻⣻⠇⢇⠧⢧⠗⢗⠷⢷⠏⢏⠯⢯⠟⢟⠿⢿⡇⣇⡧⣧⡗⣗⡷⣷⡏⣏⡯⣯⡟⣟⡿⣿".split("");
+
+function getTimestampDigits() {
+  const timestamp = Date.now();
+  const digits = [];
+  let remaining = timestamp;
+  
+  while (remaining > 0) {
+    digits.unshift(remaining % 256);
+    remaining = Math.floor(remaining / 256);
+  }
+  
+  return digits;
+}
+
+function updateClock() {
+  const clockElement = document.getElementById('clock');
+  const digits = getTimestampDigits();
+  
+  clockElement.innerHTML = digits
+    .map(d => `<span class="digit">${brailleDigits[d]}</span>`)
+    .join('');
+}
+
+// Update every millisecond (1000x speed)
+setInterval(updateClock, 1);
+updateClock(); // Initial update
