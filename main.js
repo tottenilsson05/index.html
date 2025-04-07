e66d0f21bd2668eac99c70b3c52a3552126b559e
tottenilsson05 <abbedalle@gmail.com>
2025-04-07 18:09:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+async function initParticles() {
+  await tsParticles.load("tsparticles", {
+    particles: {
+      number: {
+        value: 50,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      },
+      color: {
+        value: "#0099ff"
+      },
+      shape: {
+        type: "circle"
+      },
+      opacity: {
+        value: 0.3,
+        random: false,
+        animation: {
+          enable: true,
+          speed: 1,
+          minimumValue: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 3,
+        random: true
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: true,
+        straight: false,
+        outModes: {
+          default: "out"
+        }
+      }
+    },
+    background: {
+      color: "#ffffff",
+      opacity: 0
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", initParticles);
