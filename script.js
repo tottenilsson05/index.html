521b68e1d67e88606dad4ab86acd0dede66896e6
tottenilsson05 <abbedalle@gmail.com>
2025-05-17 20:48:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+// No specific JavaScript functionality is required for this initial setup.
+// This file is included for potential future interactivity.
+
+console.log("FOG WIF CIG page loaded successfully!");
+
