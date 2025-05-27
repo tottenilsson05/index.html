186336c4d73c29810b5d1a4bf5faf392b467b4ce
tottenilsson05 <abbedalle@gmail.com>
2025-05-27 18:47:20 +0200

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-// No specific JavaScript functionality is required for this initial setup.
-// This file is included for potential future interactivity.
-
-console.log("FOG WIF CIG page loaded successfully!");
-
