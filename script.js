25d894aae52c5a5d23927375f8d5270e2970fd93
tottenilsson05 <abbedalle@gmail.com>
2025-06-01 21:47:17 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-console.log("TIMO website loaded successfully!");
-// All animations are handled by CSS for optimal performance and simplicity.
-// No JavaScript interactions are required for the current design.
-
