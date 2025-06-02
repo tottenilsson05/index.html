653cacf19ee3e3884f0cebe9dc79f2613e9b6817
tottenilsson05 <abbedalle@gmail.com>
2025-06-02 21:12:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// This script is currently a placeholder.
+// Future JavaScript for interactivity or more complex animations can be added here.
+console.log("Feggy website script loaded!");
+
+// Example: Add a small interaction if desired
+// document.addEventListener('DOMContentLoaded', () => {
+//   const logo = document.getElementById('logo');
+//   if (logo) {
+//     logo.addEventListener('click', () => {
+//       alert('Feggy clicked!');
+//     });
+//   }
+// });
+
